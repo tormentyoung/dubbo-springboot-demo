eb51265ccd22fb5054f8c370e52e4242c97fdc83
xiongxuebing <tormentyoung@gmail.com>
2021-03-21 22:28:21 +0800

    demo优化

--- 02.springboot-dubbo-demo/springboot-dubbo-demo-consumer/src/main/java/com/xxb/springboot/dubbo/demo/consumer/controller/HelloController.java
+++ 02.springboot-dubbo-demo/springboot-dubbo-demo-consumer/src/main/java/com/xxb/springboot/dubbo/demo/consumer/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.xxb.springboot.dubbo.demo.consumer.controller;
 
 import com.xxb.springboot.dubbo.demo.api.service.HelloService;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,11 @@ public class HelloController {
     @DubboReference
     private HelloService helloService;
 
+    @Value("${spring.application.name}")
+    private String serviceName;
+
     @GetMapping("/sayHello")
     public String sayHello(String name){
-        return helloService.sayHello(name);
+        return String.format("[%s] : Hello, %s", serviceName, name);
     }
 }

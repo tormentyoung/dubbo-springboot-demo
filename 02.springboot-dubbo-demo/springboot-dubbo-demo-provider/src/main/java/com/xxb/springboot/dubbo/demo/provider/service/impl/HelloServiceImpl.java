7af90b2544967372dad8cf9460897e3f42e34481
xiongxuebing <tormentyoung@gmail.com>
2021-03-21 23:07:37 +0800

    demo优化

--- 02.springboot-dubbo-demo/springboot-dubbo-demo-provider/src/main/java/com/xxb/springboot/dubbo/demo/provider/service/impl/HelloServiceImpl.java
+++ 02.springboot-dubbo-demo/springboot-dubbo-demo-provider/src/main/java/com/xxb/springboot/dubbo/demo/provider/service/impl/HelloServiceImpl.java
@@ -2,13 +2,18 @@ package com.xxb.springboot.dubbo.demo.provider.service.impl;
 
 import com.xxb.springboot.dubbo.demo.api.service.HelloService;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @DubboService
 public class HelloServiceImpl implements HelloService {
+
+    @Value("${spring.application.name}")
+    private String serviceName;
+
     @Override
     public String sayHello(String name) {
-        return "Hello " + name;
+        return String.format("[%s] : Hello, %s", serviceName, name);
     }
 }

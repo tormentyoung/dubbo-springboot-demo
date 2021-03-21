9d601eac3ac7f7b55bf444c56cf7c4a7bed56e93
xiongxuebing <tormentyoung@gmail.com>
2021-03-21 20:54:42 +0800

    demo提交

--- 01.springboot-demo/src/main/java/com/xxb/springboot/DemoApplication.java
+++ 01.springboot-demo/src/main/java/com/xxb/springboot/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.demo.springboot;
+package com.xxb.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- 01.springboot-demo/src/main/java/com/xxb/springboot/controller/DemoController.java
+++ 01.springboot-demo/src/main/java/com/xxb/springboot/controller/DemoController.java
@@ -1,4 +1,4 @@
-package com.demo.springboot.controller;
+package com.xxb.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;

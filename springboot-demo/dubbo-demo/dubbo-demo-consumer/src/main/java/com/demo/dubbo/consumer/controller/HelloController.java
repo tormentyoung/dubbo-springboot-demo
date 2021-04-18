bfa0c60c3ade49a0c0eee9357474aaa09e2375b9
xiongxuebing <tormentyoung@gmail.com>
2021-04-18 20:41:23 +0800

    项目重构

--- springboot-demo/dubbo-demo/dubbo-demo-consumer/src/main/java/com/demo/dubbo/consumer/controller/HelloController.java
+++ springboot-demo/dubbo-demo/dubbo-demo-consumer/src/main/java/com/demo/dubbo/consumer/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.demo.dubbo.consumer.controller;
 import com.demo.dubbo.api.service.HelloService;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,7 @@ public class HelloController {
     private HelloService helloService;
 
     @GetMapping("/sayHello")
-    public String sayHello(String name){
+    public String sayHello(@RequestParam String name){
         return helloService.sayHello(name);
     }
 }
--- springboot-demo/initialize-demo/src/main/java/com/demo/springboot/controller/DemoController.java
+++ springboot-demo/initialize-demo/src/main/java/com/demo/springboot/controller/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    @GetMapping("/hello")
+    @GetMapping("/sayHello")
     public String sayHello(@RequestParam String name){
         return "hello, " + name;
     }
